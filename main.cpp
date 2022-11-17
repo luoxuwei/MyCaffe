4e67c02c740fd1e09f9e182264f9fd5be31bb14c
luoxuwei <xwluo@foxmail.com>
2022-11-18 05:28:54 +0800

    整理注释

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@
 
 using namespace std;
 
-int ReverseInt(int i)  ////把大端数据转换为我们常用的小端数据 （大小端模式的原因）
+/*转换字节序，加载mnist数据集时，需要把大端数据转换为我们常用的小端数据*/
+int ReverseInt(int i)
 {
-    unsigned char ch1, ch2, ch3, ch4;  //一个int有4个char
+    unsigned char ch1, ch2, ch3, ch4;  //int的4个字节
     ch1 = i & 255;
     ch2 = (i >> 8) & 255;
     ch3 = (i >> 16) & 255;
@@ -26,7 +27,7 @@ void ReadMnistData(string path, shared_ptr<Blob> &images)
     if (file.is_open())
     {
         //mnist原始数据文件中32位的整型值是大端存储，C/C++变量是小端存储，所以读取数据的时候，需要对其进行大小端转换!!!!
-        //1.从文件中获知魔术数字（一般都是起到标识的作用，比如用来判断这个文件是不是MNIST里面的train-labels.idx1-ubyte文件），图片数量和图片宽高信息
+        //1.从文件中获知魔术数字图片数量和图片宽高信息
         int magic_number = 0;
         int number_of_images = 0;
         int n_rows = 0;
@@ -44,7 +45,7 @@ void ReadMnistData(string path, shared_ptr<Blob> &images)
         n_cols = ReverseInt(n_cols);
         cout << "n_cols=" << n_cols << endl;
 
-        //2.将图片转为Blob存储！
+        //2.将图片转为Blob存储
         for (int i = 0; i<number_of_images; ++i)  //遍历所有图片
         {
             for (int h = 0; h<n_rows; ++h)   //遍历高
@@ -53,10 +54,7 @@ void ReadMnistData(string path, shared_ptr<Blob> &images)
                 {
                     unsigned char temp = 0;
                     file.read((char*)&temp, sizeof(temp));      //读入一个像素值！
-                    //-----将temp中的数据写入Blob中------
                     (*images)[i](h, w, 0) = (double)temp / 255; //除以255，做归一化
-                    //-----------------------------------
-
                 }
             }
         }
@@ -82,15 +80,13 @@ void ReadMnistLabel(string path, shared_ptr<Blob> &labels)
         file.read((char*)&number_of_images, sizeof(number_of_images));
         number_of_images = ReverseInt(number_of_images);
         cout << "number_of_Labels=" << number_of_images << endl;
-        //2.将所有标签转为Blob存储！（手写数字识别：0~9）
+        //2.将所有标签转为Blob存储（手写数字识别：0~9）
         for (int i = 0; i<number_of_images; ++i)
         {
             unsigned char temp = 0;
             file.read((char*)&temp, sizeof(temp));
-            //-----将temp中的数据写入Blob中------
+            //one-hot
             (*labels)[i](0, 0, (int)temp) = 1;
-
-            //-----------------------------------
         }
     }
     else
@@ -101,7 +97,7 @@ void ReadMnistLabel(string path, shared_ptr<Blob> &labels)
 
 void trainModel(string configFile, shared_ptr<Blob> X, shared_ptr<Blob> Y)
 {
-    //0. 读入并解析我们的网络结构定义
+    //0. 读入并解析网络结构定义
     NetParameter net_param;
     net_param.readNetParam(configFile);
 
--- net.cpp
+++ net.cpp
@@ -12,31 +12,33 @@ void NetParameter::readNetParam(std::string file) {
     ifstream ifs;
     ifs.open(file);
     assert(ifs.is_open());
-    Json::Reader reader;  //  解析器
-    Json::Value value;      //存储器,解析器解析出来的值就存在value对象里
+    Json::Reader reader;  /*解析器*/
+    Json::Value value;      /*存储器,解析器解析出来的值就存在value对象里*/
     if (reader.parse(ifs, value))
     {
         if (!value["train"].isNull())
         {
-            auto &tparam = value["train"];  //拿到“train”对象里面的所有元素
-            this->lr = tparam["learning rate"].asDouble(); //解析成Double类型存放
+            /*拿到train对象里面的所有元素*/
+            auto &tparam = value["train"];
+            this->lr = tparam["learning rate"].asDouble();
             this->lr_decay = tparam["lr decay"].asDouble();
-            this->update = tparam["update method"].asString();//解析成String类型存放
+            this->update = tparam["update method"].asString();
             this->momentum = tparam["momentum parameter"].asDouble();
-            this->num_epochs = tparam["num epochs"].asInt();//解析成Int类型存放
-            this->use_batch = tparam["use batch"].asBool();//解析成Bool类型存放
+            this->num_epochs = tparam["num epochs"].asInt();
+            this->use_batch = tparam["use batch"].asBool();
             this->batch_size = tparam["batch size"].asInt();
             this->eval_interval = tparam["evaluate interval"].asInt();
             this->lr_update = tparam["lr update"].asBool();
             this->snap_shot = tparam["snapshot"].asBool();
             this->snapshot_interval = tparam["snapshot interval"].asInt();
             this->fine_tune = tparam["fine tune"].asBool();
-            this->preTrainModel = tparam["pre train model"].asString();//解析成String类型存放
+            this->preTrainModel = tparam["pre train model"].asString();
         }
         if (!value["net"].isNull())
         {
-            auto &nparam = value["net"];                                //“net”数组里面的对象是所有的网络层
-            for (int i = 0; i < (int)nparam.size(); ++i)                //遍历“net”数组里面的所有对象
+            /*net数组里面的对象是所有的网络层*/
+            auto &nparam = value["net"];
+            for (int i = 0; i < (int)nparam.size(); ++i)
             {
                 auto &ii = nparam[i];
                 this->layers.push_back(ii["name"].asString());  //层名称
@@ -80,41 +82,43 @@ void NetParameter::readNetParam(std::string file) {
 
 void Net::initNet(NetParameter& param, vector<shared_ptr<Blob>>& X, vector<shared_ptr<Blob>>& Y)
 {
-    //1.打印层结构
-    layers_ = param.layers;   // 层名，param.layers类型为vector<string>
-    ltypes_ = param.ltypes;    // 层类型 , param.ltypes类型为vector<string>
+    /*层名*/
+    layers_ = param.layers;
+    /*层类型*/
+    ltypes_ = param.ltypes;
     for (int i = 0; i < layers_.size(); ++i)
     {
         cout << "layer = " << layers_[i] << " ; " << "ltype = " << ltypes_[i] << endl;
     }
-    //2.初始化Net类相关成员变量
+    /*数据集*/
     X_train_ = X[0];
     Y_train_ = Y[0];
     X_val_ = X[1];
     Y_val_ = Y[1];
-
-    for (int i = 0; i < (int)layers_.size(); ++i)   //遍历每一层
+    /*遍历每一层*/
+    for (int i = 0; i < (int)layers_.size(); ++i)
     {
-        data_[layers_[i]] = vector<shared_ptr<Blob>>(3, NULL);    //为每一层创建前向计算要用到的3个Blob
-        diff_[layers_[i]] = vector<shared_ptr<Blob>>(3, NULL);      //为每一层创建反向计算要用到的3个Blob
-        outShapes_[layers_[i]] = vector<int>(4);       //定义缓存，存储每一层的输出尺寸
+        /*为每一层创建前向计算要用到的3个Blob x w b*/
+        data_[layers_[i]] = vector<shared_ptr<Blob>>(3, NULL);
+        /*为每一层创建反向计算要用到的3个Blob dw db */
+        diff_[layers_[i]] = vector<shared_ptr<Blob>>(3, NULL);
+        /*存储每一层的输出尺寸*/
+        outShapes_[layers_[i]] = vector<int>(4);
     }
 
-    //3. 完成每一层的W和b的初始化
+    /*完成每一层的W和b的初始化*/
     shared_ptr<Layer> myLayer(NULL);
     vector<int> inShape = { param.batch_size,
                             X_train_->get_C(),
                             X_train_->get_H(),
                             X_train_->get_W() };
     cout << "input -> (" << inShape[0] << ", " << inShape[1] << ", " << inShape[2] << ", " << inShape[3] << ")" << endl;
-    /*最后一层softmax层，没什么初始化的，所以代码里减去了这一层*/
-    for (int i = 0; i < (int)layers_.size()-1; ++i)   //遍历每一层
+    /*遍历每一层，最后一层softmax层，没什么初始化的，所以代码里减去了这一层*/
+    for (int i = 0; i < (int)layers_.size()-1; ++i)
     {
         string lname = layers_[i];
         string ltype = ltypes_[i];
         //conv1->relu1->pool1->fc1->softmax
-        //1. 代码耦合很紧，添加新的层类型比较困难，需要改动的地方比较多。
-        //2. 层对象是局部对象，下次想要调用层里面的方法，必须重新新建对象。
         if (ltype == "Conv")
         {
             myLayer.reset(new ConvLayer);
@@ -132,8 +136,11 @@ void Net::initNet(NetParameter& param, vector<shared_ptr<Blob>>& X, vector<share
             myLayer.reset(new FcLayer);
         }
         myLayers_[lname] = myLayer;
+        /*初始化参数*/
         myLayer->initLayer(inShape, lname, data_[lname], param.lparams[lname]);
+        /*计算维度*/
         myLayer->calcShape(inShape, outShapes_[lname], param.lparams[lname]);
+        /*本层的输入是前层的输出*/
         inShape.assign(outShapes_[lname].begin(), outShapes_[lname].end());
         cout << lname << "->(" << outShapes_[lname][0] << "," << outShapes_[lname][1] << "," << outShapes_[lname][2] << "," << outShapes_[lname][3] << ")" << endl;
     }
